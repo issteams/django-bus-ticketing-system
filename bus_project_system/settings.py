d56d364f3d2376e646dbfc006e6752fa5fc30c01
codesmith-abba <abdulmumina535@gmail.com>
2024-03-04 02:10:32 +0100

    rename the app name from bus_tecketing_app to bus_ticketing_app

--- bus_project_system/settings.py
+++ bus_project_system/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "bus_ticketing_app",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
--- bus_ticketing_app/apps.py
+++ bus_ticketing_app/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class BusTecketingAppConfig(AppConfig):
+class BusTicketingAppConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "bus_tecketing_app"
+    name = "bus_ticketing_app"
