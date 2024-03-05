8578bf552997e7303c436cd5931fc9e7507ddef3
codesmith-abba <abdulmumina535@gmail.com>
2024-03-05 09:07:00 +0100

    Added Models

--- bus_ticketing_app/admin.py
+++ bus_ticketing_app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Passenger, BusCompanyStaff, Admin, Bus, BusRoute, Ticket
+
+admin.site.register(Passenger)
+admin.site.register(BusCompanyStaff)
+admin.site.register(Admin)
+admin.site.register(Bus)
+admin.site.register(BusRoute)
+admin.site.register(Ticket)
--- bus_ticketing_app/models.py
+++ bus_ticketing_app/models.py
@@ -1,3 +1,63 @@
 from django.db import models
 
 # Create your models here.
+class Passenger(models.Model):
+    name = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=255)
+    phone_number = models.CharField(max_length=20, blank=True, null=True)
+    address = models.CharField(max_length=255, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+class BusCompanyStaff(models.Model):
+    name = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=255)
+    position = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+class Admin(models.Model):
+    name = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+class Bus(models.Model):
+    company = models.ForeignKey(BusCompanyStaff, on_delete=models.CASCADE)
+    route = models.ForeignKey('BusRoute', on_delete=models.CASCADE)
+    departure_time = models.DateTimeField()
+    arrival_time = models.DateTimeField()
+    total_seats = models.IntegerField()
+    available_seats = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.company.name} - {self.route.origin} to {self.route.destination}"
+
+    class Meta:
+        verbose_name_plural = 'buses'
+
+
+class BusRoute(models.Model):
+    origin = models.CharField(max_length=255)
+    destination = models.CharField(max_length=255)
+    distance = models.FloatField()
+    duration = models.DurationField()
+
+    def __str__(self):
+        return f"{self.origin} to {self.destination}"
+
+class Ticket(models.Model):
+    passenger = models.ForeignKey(Passenger, on_delete=models.CASCADE)
+    bus = models.ForeignKey(Bus, on_delete=models.CASCADE)
+    departure_time = models.DateTimeField()
+    arrival_time = models.DateTimeField()
+    status = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.passenger.name} - {self.bus.company.name} - {self.departure_t}"
